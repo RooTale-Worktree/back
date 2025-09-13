f252972785eb5aa1a04d8c0f5cfe781fdcaa103f
eastlight82 <bigstake82@gmail.com>
2025-09-13 18:57:10 +0900

    feat project

---
+++ src/main/java/com/LiveToon/LiveToonApplication.java
@@ -0,0 +1,13 @@
+package com.LiveToon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveToonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveToonApplication.class, args);
+	}
+
+}
