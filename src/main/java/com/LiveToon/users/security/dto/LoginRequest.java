fc406decbd12a3b1cb910264537959db8db720b8
eastlight82 <bigstake82@gmail.com>
2025-09-21 16:59:51 +0900

    feat /auth (login, signup/ by jwt)

---
+++ src/main/java/com/LiveToon/users/security/dto/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.LiveToon.users.security.dto;
+
+public record LoginRequest(String username, String password) {
+}
---
+++ src/main/java/com/LiveToon/users/security/dto/SignupRequest.java
@@ -0,0 +1,4 @@
+package com.LiveToon.users.security.dto;
+
+public record SignupRequest(String username, String password, String name) {
+}
